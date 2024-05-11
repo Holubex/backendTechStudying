3896afcad1feae202451074e93249f659fdaa16f
User <kubo.holubek@gmail.com>
2024-05-11 12:59:34 +0200

    Added hello world.

--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+# view definovana pomococu funkcie
+def hello(request):
+    return HttpResponse("Hello World!")
+
+def hello2(request, s):
+    return HttpResponse(f'Hello {s} world!')
+
+
+#URL encoding
+def hello3(request):
+    s = request.GET.get('s', '')
+    return HttpResponse(f'Hello {s} world!')
